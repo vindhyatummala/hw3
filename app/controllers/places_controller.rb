30437ab01e70762886b23d73ceb9c369d8e17779
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-02-15 22:28:26 +0000

    Completing assignment

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,22 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+      end
+
+      def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+
+end
