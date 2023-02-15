e921b7fdae8d50a9db8cb199e30c02d7b2c267b3
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-02-15 22:39:55 +0000

    Completing the assignment

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,21 +2,38 @@ class PlacesController < ApplicationController
 
     def index
         @places = Place.all
-      end
+    end
     
-      def new
+    def new
         @place = Place.new
-      end
+    end
     
-      def show
+    def show
         @place = Place.find_by({ "id" => params["id"] })
-      end
+    end
 
-      def create
+    def create
         @place = Place.new
         @place["name"] = params["place"]["name"]
         @place.save
         redirect_to "/places"
-      end
+    end
 
+    def edit
+        @place = Place.find_by({ "id" => params["id"] })
+    end
+    
+    def update
+        @place = Place.find_by({ "id" => params["id"] })
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+    end
+    
+    def destroy
+        @place = Place.find_by({ "id" => params["id"] })
+        @place.destroy
+        redirect_to "/places"
+    end
+    
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,17 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-      end
+    end
     
-      def new
+    def new
         @post = Post.new
-      end
+    end
     
-      def show
+    def show
         @post = Post.find_by({ "id" => params["id"] })
-      end
+    end
 
-      def create
+    def create
         @post = Post.new
         @post["title"] = params["post"]["title"]
         @post["description"] = params["post"]["description"]
@@ -20,6 +20,26 @@ class PostsController < ApplicationController
         @post["place_id"] = params["post"]["place_id"]
         @post.save
         redirect_to "/posts"
-      end
+    end
+
+    def edit
+        @post = Post.find_by({ "id" => params["id"] })
+    end
+    
+    def update
+        @post = Post.find_by({ "id" => params["id"] })
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/posts"
+    end
+    
+    def destroy
+        @post = Post.find_by({ "id" => params["id"] })
+        @post.destroy
+        redirect_to "/posts"
+    end
 
 end
